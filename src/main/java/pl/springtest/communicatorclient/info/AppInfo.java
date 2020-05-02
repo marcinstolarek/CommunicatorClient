55ceb7d7cdf7e30f0478397bc95049b7b6a20b73
marcinstolarek <marcinstolarek>
2020-05-02 21:10:09 +0200

    some changes

---
+++ src/main/java/pl/springtest/communicatorclient/info/AppInfo.java
@@ -0,0 +1,6 @@
+package pl.springtest.communicatorclient.info;
+
+public abstract class AppInfo {
+    public static final String VERSION_INFO = "1.0.0";
+    public static final String AUTHOR = "Marcin Stolarek";
+}
